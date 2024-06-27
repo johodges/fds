9206905409f0d68999da6d36ff6a82a0ea3d50c2
Jonathan Hodges <johodges@vt.edu>
2024-06-27 15:15:34 -0400

    add parameters for passive scalar transport of zeta

--- Source/cons.f90
+++ Source/cons.f90
@@ -409,7 +409,7 @@ REAL(EB) :: FIXED_MIX_TIME=-1._EB                                   !< User-spec
 REAL(EB) :: INITIAL_UNMIXED_FRACTION=1._EB                          !< Initial amount of mixed air-fuel in combustion chamber
 REAL(EB) :: GLOBAL_ODE_REL_ERROR=1.E-4_EB                           !< Error tolerance in Richardson extrapolation
 REAL(EB) :: H_F_REFERENCE_TEMPERATURE=25._EB                        !< Heat of formation reference temperature (C->K)
-REAL(EB) :: EXT_MIN_ENTHALPY=0.1                                    !< Fraction below H_CRIT for full extinction
+REAL(EB) :: EXT_MIN_TEMP_OFFSET=100._EB                             !< Temperature below T_CRIT for full extinction
 REAL(EB) :: FREE_BURN_TEMPERATURE=600._EB                           !< Temperature above which fuel and oxygen burn freely (C->K)
 REAL(EB) :: FINITE_RATE_MIN_TEMP=-273.15                            !< When FR is present, min temp. to compute combustion (C->K)
 REAL(FB) :: HRRPUV_MAX_SMV=1200._FB                                 !< Clipping value used by Smokeview (kW/m3)
@@ -431,6 +431,7 @@ INTEGER :: N_FIXED_CHEMISTRY_SUBSTEPS=-1                            !< Number of
 LOGICAL :: OUTPUT_CHEM_IT=.FALSE.
 LOGICAL :: REAC_SOURCE_CHECK=.FALSE.
 LOGICAL :: COMPUTE_ADIABATIC_FLAME_TEMPERATURE=.FALSE.              !< Report adiabatic flame temperature per REAC in LU_OUTPUT
+LOGICAL :: MIXING_AS_PASSIVE_SCALAR_TRANSPORT=.FALSE.               !< Specify if unmixed fraction should be calculated as a passive scalar
 
 REAL(EB) :: RSUM0                                     !< Initial specific gas constant, \f$ R \sum_i Z_{i,0}/W_i \f$
 
