e5b95a665b6493fea7d2229c7b8766e1c409555f
Hodges <jhodges@jensenhughes.com>
2025-03-20 17:37:52 -0400

    add copy user guide aux file for vnv guides

--- Manuals/FDS_Validation_Guide/make_guide.py
+++ Manuals/FDS_Validation_Guide/make_guide.py
@@ -1,9 +1,18 @@
-import os, subprocess, sys
+import os, subprocess, sys, shutil
 
 guide = 'FDS_Validation_Guide'
 firemodels = os.path.join(os.path.dirname(__file__),'..','..','..')
 manual_dir = os.path.join(firemodels,'fds','Manuals',guide)
 command = 'python ' + os.path.join(firemodels,'fds','Manuals','scripts','make_guide.py') + ' --file %s --clean'%(guide)
+
+userguide_aux = os.path.join(firemodels, 'fds','Manuals','FDS_User_Guide', 'FDS_User_Guide.aux')
+valguide_aux = os.path.join(firemodels, 'fds','Manuals','FDS_Validation_Guide', 'FDS_User_Guide.aux')
+
+if os.path.exists(userguide_aux) is False:
+    print("***warning: $AUXUSER does not exist. Build the FDS User's")
+    print("            Guide before building the FDS Validation Guide")
+else:
+    shutil.copy2(userguide_aux, valguide_aux)
 
 p = subprocess.Popen(command, cwd=manual_dir, stdout=sys.stdout, stderr=sys.stderr, shell=True, close_fds=True, env=os.environ)
 txt = p.communicate()
